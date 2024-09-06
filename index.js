35943a7168b4c26fde1bdd3596aeb55be431d22e
Martin-Norton <martinnorton07@gmail.com>
2024-09-06 10:18:33 -0300

    aso a public style

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const MET_API_URL = 'https://collectionapi.metmuseum.org/public/collection/v1';
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
     res.render('index');
